47fe9224f794b1eaf0ca5f2a78ebac8ea695ae31
Faid Terence JABO <faidterence@outlook.com>
2023-02-27 00:26:05 +0200

    folders

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Typing Animation
+var typed =new Typed (".typing", {
+    strings: ["", "Software Engineer", "Full stack Developer", "Web Designer"],
+    typespeed:60,
+    Backspeed:20,
+    loop:true
+})
+
+
+
+
+
+
+
